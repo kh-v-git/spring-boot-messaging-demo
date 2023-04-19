383545e067ad636dae5c29efa7fa59b7265a4acc
Vadym Khomenko <vadym_khomenko@epam.com>
2023-04-19 20:08:35 +0200

    Initial commit.

---
+++ src/main/java/com/khomenko/eventrest/domain/Event.java
@@ -0,0 +1,7 @@
+/*
+ * Do not reproduce without permission in writing.
+ * Copyright (c) 2023.
+ */
+package com.khomenko.eventrest.domain;
+
+public class Event {}
---
+++ src/test/java/com/khomenko/eventrest/EventRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khomenko.eventrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
